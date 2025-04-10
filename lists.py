1ccd4616e7cccb99929c19873d66c5a2ee83f8df
bluehawaii02 <daltonkimutai02@gmail.com>
2025-04-10 09:54:03 +0300

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,13 @@
+my_list=[]
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(1, 15)
+list=[50,60,70]
+my_list.extend(list)
+my_list.remove(70)
+my_list.sort()
+i_30=my_list.index(30)
+print(i_30)
+print(my_list)
